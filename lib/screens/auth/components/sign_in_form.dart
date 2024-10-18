22e50d2a642858aa1b015e64d4c88ef52c7b11f8
namnguyen168168 <hoangnam12696@gmail.com>
2024-10-18 19:42:34 +0700

    update project

---
+++ lib/screens/auth/components/sign_in_form.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../../findRestaurants/find_restaurants_screen.dart';
+
+import '../../../constants.dart';
+import '../forgot_password_screen.dart';
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({super.key});
+
+  @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? _email;
+  String? _password;
+  bool _obscureText = true;
+
+  Future<void> _signIn() async {
+    final url = 'http://10.0.2.2:5454/auth/signin'; // For Android Emulator // Replace with your API endpoint
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'email': _email,
+        'password': _password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // Handle success
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const FindRestaurantsScreen(),
+        ),
+            (_) => false,
+      );
+    } else {
+      var message = "Wrong password";
+      // Handle error
+      _showErrorDialog(message);
+    }
+  }
+
+// Function to show error dialog
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Sign In Failed'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            validator: emailValidator.call,
+            onSaved: (value) {
+              _email = value;
+            },
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: "Email Address"),
+          ),
+          const SizedBox(height: defaultPadding),
+
+          // Password Field
+          TextFormField(
+            obscureText: _obscureText,
+            validator: passwordValidator.call,
+            onSaved: (value) {
+              _password = value;
+            },
+            decoration: InputDecoration(
+              hintText: "Password",
+              suffixIcon: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+                child: _obscureText
+                    ? const Icon(Icons.visibility_off, color: bodyTextColor)
+                    : const Icon(Icons.visibility, color: bodyTextColor),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+
+          // Forget Password
+          GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ForgotPasswordScreen(),
+              ),
+            ),
+            child: Text(
+              "Forget Password?",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(fontWeight: FontWeight.w500),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+
+          // Sign In Button
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+                _signIn(); // Call the sign-in function
+              }
+            },
+            child: const Text("Sign in"),
+          ),
+        ],
+      ),
+    );
+  }
+}
